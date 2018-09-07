6b9e0db3eea9dfd9e3a03a8ab6574845c509dc1a
Mike <m.consemulder1992@gmail.com>
2018-09-07 15:36:31 +0200

    and the server thingy

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express()
 
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
 const port = process.env.PORT || 80
 console.log('listening to port:', port)
 app.listen(port)
